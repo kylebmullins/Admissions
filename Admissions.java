55105eeb4d1705b878d8d6890d877691b7f390a1
Kyle Mullins <kylebmullins@gmail.com>
2020-09-25 11:09:03 -0600

    Add .gitignore

--- Admissions.java
+++ Admissions.java
@@ -7,6 +7,18 @@ public class Admissions {
         String surname = askString("What is the student's last name?");
         String firstname = askString("What wis the student's first name?");
         String residence = askString("In which state does the student currently reside?");
+        int SATPass = testInt(SAT, 1500, 1400, 1200);
+        int rankPass = testInt(rank, 0, 0, 0);
+        int awardsPass = testInt(rank, 0, 0, 0);
+        int surnamePass = testSurname(surname);
+        int residencePass = testResidence(residence);
+        int pass = (SATPass + rankPass + awardsPass + surnamePass + residencePass);
+        boolean admitted = false;
+        if (pass >= 2) {
+            admitted = true;
+        } else if (pass < 2) {
+            admitted = false;
+        }
     }
 
     public static int askInt(String question) {
@@ -25,10 +37,37 @@ public class Admissions {
         return ask;
     }
 
-    public static int testElite(int value, int condition, int acceptCondition) {
+    public static int testInt(int value, int condition, int acceptCondition, int failCondition) {
         int back;
         if (value >= condition) {
-            back = 8;
+            back = 2;
+        } else if (value >= acceptCondition) {
+            back = 1;
+        } else if (value < failCondition) {
+            back = -10;
+        } else {
+            back = 0;
         }
+        return back;
+    }
+
+    public static int testSurname(String surname) {
+        int back;
+        if (surname.equals("Padjen")) {
+            back = 2;
+        } else {
+            back = 0;
+        }
+        return back;
+    }
+
+    public static int testResidence(String residence) {
+        int back = 0;
+        if (residence.equals("Colorado")) {
+            back = 2;
+        } else if (residence.equals("Florida")) {
+            back = -10;
+        }
+        return back;
     }
 }
